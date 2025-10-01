cec65f707005c9aac16f2a216d0d7549ae015248
Abhilash Kolasani <nannyabhi9@gmail.com>
2025-10-01 20:08:13 +0530

    feat: add numbers with default delimiter \n
    Implemented addition of numbers with default delimiters.

--- src/main/java/org/tdd/Main.java
+++ src/main/java/org/tdd/Main.java
@@ -8,13 +8,13 @@ public class Main {
         int resultWithSpaceString = stringCalculator.add(" ");
         int resultWithOneNumber = stringCalculator.add("1");
         int resultWithTwoNumbers = stringCalculator.add("1,2");
+        int resultWithDefaultDelimiter = stringCalculator.add("//;\n1;2;3;4");
 
         System.out.println("resultWithNull: " + resultWithNull);
         System.out.println("resultWithEmptyString: " + resultWithEmptyString);
-        System.out.println("resultWithEmptyString: " + resultWithSpaceString);
-        System.out.println("resultWithEmptyString: " + resultWithOneNumber);
-        System.out.println("resultWithEmptyString: " + resultWithTwoNumbers);
-
-
+        System.out.println("resultWithSpaceString: " + resultWithSpaceString);
+        System.out.println("resultWithOneNumber: " + resultWithOneNumber);
+        System.out.println("resultWithTwoNumbers: " + resultWithTwoNumbers);
+        System.out.println("resultWithDefaultDelimiter: " + resultWithDefaultDelimiter);
     }
 }
